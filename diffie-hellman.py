712bf2983534dbe4a05b8c66aa5af0d82b2ab966
Ricardo Parizotto <ricardo.dparizotto@gmail.com>
2016-08-03 15:01:45 -0300

    Create diffie-hellman.py

---
+++ diffie-hellman.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python2.5
+# -*- coding:utf-8 -*-
+
+M = 1099511627776
+B = 3
+
+def diffie_hellman (base, exp):
+    return (base ** exp)%M; 
+
+def ext_key ( k ):
+    return diffie_hellman(B, k)
+
+def check_key( alice, bob ):
+    print ('Alice key:', diffie_hellman(ext_key(bob), alice))
+    print ('Bob key:', diffie_hellman(ext_key(alice), bob))
+ 
+if __name__ == '__main__':
+    alice = int(input('type alice key:\n'))
+    bob = int(input('type bob key:\n'))
+    check_key(alice, bob)
