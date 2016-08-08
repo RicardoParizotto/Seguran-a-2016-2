858ce76c74552d201295dab4ebf212b8b5f8bce8
ricardoparizotto <ricardo.dparizotto@gmail.com>
2016-08-08 15:08:14 -0300

    RSA

--- AES/aes.py
+++ AES/aes.py
@@ -1,30 +1,49 @@
 #!/usr/bin/env python3
 
+# fontes: 
+# https://pypi.python.org/pypi/pycrypto
+# https://stuvel.eu/python-rsa-doc/usage.html
+
+import rsa
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
-opcao = input("1 - Encriptar um arquivo\n2 - Decriptar um arquivo\n> ")
-fname = input("Digite o nome do arquivo: ")
-
-chave = input("Digite uma chave para encriptar/decriptar o texto: ")
-chave = chave + ''.join(['0' for i in range(16-len(chave)%16)])
-
-if opcao == '1':
-    # Adaptado de https://pypi.python.org/pypi/pycrypto
-    obj = AES.new(chave, AES.MODE_CBC, 'This is an IV456')
-    f = open(fname)
-    message = b64encode(f.read().encode('utf-8'))
-    message = message + bytes(''.join(['$' for i in range(16-len(message)%16)]).encode('utf-8'))
-    ciphertext = obj.encrypt(message)
-    print("Texto encriptado: %s"%ciphertext)
+def encrypt( file_, pub_key ):
+    aes_key = rsa.randnum.read_random_bits(128) 
+    obj = AES.new(aes_key, AES.MODE_CBC, 'This is an IV456')
+    msg = b64encode(file_.read().encode('utf-8'))
+    msg = msg + bytes(''.join(['$' for i in range(16-len(msg)%16)]).encode('utf-8'))
     f = open("enc01","wb")
-    f.write(ciphertext)
-elif opcao == '2':
-    f = open(fname,"rb")
-    ciphertext = f.read()
-    obj2 = AES.new(chave, AES.MODE_CBC, 'This is an IV456')
-    texto = obj2.decrypt(ciphertext)
-    strt = str(texto).split('$')[0].split("'")[1]
-    texto = b64decode(strt)
+    f.write(obj.encrypt(msg))
+    return rsa.encrypt(aes_key, pub_key)
+
+    
+def decrypt( file_, aes_key ):
+    ciphertext = file_.read()
+    obj = AES.new(aes_key, AES.MODE_CBC, 'This is an IV456')
+    msg = obj.decrypt(ciphertext)
+    msg = b64decode(msg)
     print("Texto original: ",end="")
-    print(texto)
+    print(msg)
+    
+
+
+if __name__ == '__main__':
+
+    rsa_k = None
+
+    (pub_key, pvt_key) = rsa.newkeys(2048)
+
+    while True : 
+        opcao = input("1 - encrypt file\n2 - decrypt file\n> ")
+        fname = input("File name: ")
+
+        if opcao == '1':
+            f = open(fname)
+            rsa_k = encrypt(f, pub_key)
+
+
+        elif opcao == '2':
+            f = open(fname,"rb")
+            decrypt(f, rsa.decrypt(rsa_k, pvt_key))
+     
